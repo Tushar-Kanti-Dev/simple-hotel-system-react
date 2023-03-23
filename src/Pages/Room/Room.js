7e0133c8fe604ea90068e6f842c24d575d5c1f33
Tushar-Kanti-Dev <62166306+Tushar-Kanti-Dev@users.noreply.github.com>
2023-03-23 23:39:35 +0600

    book now button added

--- src/Pages/Room/Room.js
+++ src/Pages/Room/Room.js
@@ -1,10 +1,19 @@
-import React from 'react';
+import React, { useState } from 'react';
 import { Card } from 'react-bootstrap';
+import './Room.css'
 
 const Room = ({room}) => {
     const {name, img ,price} = room;
+
+    const [items, setItems] = useState({})
+
+    const handleBookRoom = (id)=>{
+        setItems(items);
+        console.log(id);
+    }
+    console.log(items);
     return (
-        <div  className='gx-2 col-md-6 col-lg-4 m-0'>
+        <div  className='gx-2 col-md-6 col-lg-4 m-0 mb-2'>
              <Card>
                 <Card.Img variant="top" src={img} />
                 <Card.Body>
@@ -14,7 +23,9 @@ const Room = ({room}) => {
                     This is a wider card with supporting text below as a natural lead-in
                     to additional content. This content is a little bit longer.
                 </Card.Text>
-
+                <div className="book-now-button text-center">
+                <button onClick={handleBookRoom} className='text-light bg-dark rounded px-5 pe-4 py-2'>Book Now</button>
+                </div>
                 </Card.Body>
             </Card>
         </div>
