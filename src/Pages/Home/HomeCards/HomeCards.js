0c3dd8843124f9ee1121305ffc350b5f1b1779ad
Tushar-Kanti-Dev <62166306+Tushar-Kanti-Dev@users.noreply.github.com>
2023-03-23 23:09:09 +0600

    view all button added in home component

--- src/Pages/Home/HomeCards/HomeCards.js
+++ src/Pages/Home/HomeCards/HomeCards.js
@@ -1,4 +1,5 @@
 import React from 'react';
+import { Link } from 'react-router-dom';
 import useRooms from '../../../hooks/useRooms';
 import HomeCard from '../HomeCard/HomeCard';
 
@@ -10,11 +11,17 @@ const HomeCards = () => {
             <h1 className='text-center'>--Welcome Our Hotel--</h1>
             <div className="row">
                 {
-                    rooms.map(room =><HomeCard
-                    key={room.id}
-                    room = {room}
-                    />)
-                }
+                    rooms.filter((product) => product.price >= 100)
+                    .map((room) => (
+                    <HomeCard 
+                    key={room.id} 
+                    room={room}>
+                </HomeCard>
+                ))
+            }
+            </div>
+            <div className="view-all-button text-center m-4">
+            <button className='bg-dark p-3 rounded'><Link className='text-white m-2 p-5' to='/rooms'>View All</Link></button>
             </div>
         </div>
     );
